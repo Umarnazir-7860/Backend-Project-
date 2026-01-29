054a9445fca33841df0848ac502bac390af291f7
Umarnazir-7860 <nazirumar@7860.gmail.com>
2026-01-29 21:01:59 +0500

    Add some logic to create the owner

--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owner-model');
+
 router.get('/', (req, res) => {
     res.send('Owner Router Working');
 });
+
+console.log(process.env.NODE_ENV);
+
+if(process.env.NODE_ENV === 'development'){
+router.post('/create', async(req,res)=>{
+     let owners = await ownerModel.find();
+     if(owners.length > 0){
+        return res.send(503).send('You Dont have permission to create more owners');
+     }
+    let {fullname,email,password} = req.body;
+    let createdOwner= await ownerModel.create({
+    fullname:fullname,
+    email:email,
+    password:password,
+    gstin:gstin
+
+    })
+    res.send('Owner Created');
+})
+}
+
 module.exports = router;
