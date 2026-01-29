fa2606388eab6737b369a45c12e5d48779fb4df6
Umarnazir-7860 <nazirumar@7860.gmail.com>
2026-01-29 20:34:14 +0500

    Create the connect with database by industrial standards

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
@@ -18,6 +19,11 @@ app.set('view engine', 'ejs');
 app.use('/users', usersRouter);
 app.use('/owners', ownersRouter);
 app.use('/products', productsRouter);
+
+app.get('/', (req, res)=>{
+    res.send('Server is Working');
+});
+
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
--- config/mongoose.connection.js
+++ config/mongoose.connection.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
+const dbgr = require('debug')('development:mongoose');
 
-
-mongoose.connect('mongodb+srv://muhammadumarnazirhussain:Zumarn7770@cluster0.zb8b9lj.mongodb.net/BackendProject')
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
-    console.log('Connected to MongoDB');
+    dbgr('Connected to MongoDB');
 })
 .catch((err) => {
-    console.log('Error connecting to MongoDB', err);
+    dbgr('Error connecting to MongoDB', err);
 });
 
 module.exports = mongoose.connection;
